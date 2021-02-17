13eec4a6057c4bed7d513c8e70e888e483ef634c
Christian <12550267+iceychris@users.noreply.github.com>
2021-02-17 16:28:46 +0100

    + setup build-sphinx pipeline

---
+++ docs/build-sphinx.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -x
+
+# setup
+apt-get update
+apt-get -y install git rsync python3-sphinx
+pip3 install -y -r requirements.txt
+
+pwd ls -lah
+export SOURCE_DATE_EPOCH=$(git log -1 --pretty=%ct)
+ 
+##############
+# BUILD DOCS #
+##############
+ 
+# Python Sphinx, configured with source/conf.py
+# See https://www.sphinx-doc.org/
+make clean
+make html
+
+#######################
+# Update GitHub Pages #
+#######################
+
+git config --global user.name "${GITHUB_ACTOR}"
+git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+ 
+docroot=`mktemp -d`
+rsync -av "build/html/" "${docroot}/"
+ 
+pushd "${docroot}"
+
+git init
+git remote add deploy "https://token:${GITHUB_TOKEN}@github.com/${GITHUB_DST_REPOSITORY}.git"
+git checkout -b ${GITHUB_BRANCH}
+ 
+# Adds .nojekyll file to the root to signal to GitHub that  
+# directories that start with an underscore (_) can remain
+touch .nojekyll
+ 
+# Copy the resulting html pages built from Sphinx to the gh-pages branch 
+git add .
+ 
+# Make a commit with changes and any new files
+msg="Updating Docs for commit ${GITHUB_SHA} made on `date -d"@${SOURCE_DATE_EPOCH}" --iso-8601=seconds` from ${GITHUB_REF} by ${GITHUB_ACTOR}"
+git commit -am "${msg}"
+ 
+# overwrite the contents of the gh-pages branch on our github.com repo
+git push deploy ${GITHUB_BRANCH} --force
+ 
+popd # return to main repo sandbox root
+ 
+# exit cleanly
+exit 0
