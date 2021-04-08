df662605e6a018719de8e765cff9365b60b9511e
Christian <12550267+iceychris@users.noreply.github.com>
2021-04-08 11:14:14 +0000

    * fix Dockerfiles

--- docker/check.py
+++ docker/check.py
@@ -1,11 +1,11 @@
-# imports?
+# imports
 import torch
-
-# import torchaudio
-import fastcore
 import fastai
+import fastcore
+import fastaudio
 
-# import fastai2_audio
-
-# mkldnn?
+# mkldnn issue?
 torch.stft(torch.randn(80, 80), 20)
+
+# loss
+from warp_rnnt import rnnt_loss
