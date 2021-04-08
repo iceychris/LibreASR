* fix gradient accumulation for single node training

libreasr/lib/callbacks.py
@@ -2,6 +2,8 @@ import contextlib
 from types import MethodType
 import traceback
 
+import torch
+
 from fastai.learner import Callback, CancelBatchException
 from fastai.callback.fp16 import MixedPrecision
 from fastai.torch_core import rank_distrib
@@ -20,9 +22,14 @@ class GradAccumCallback(Callback):
         self.num_batches = num_batches
 
     def after_backward(self):
+        # increment
+        self.count += 1
+
         # skip opt step if applicable
         if (self.count % self.num_batches) == 0:
             # do optimizer step
+            # if (self.count % 200) == 0:
+            #     print(torch.cuda.memory_summary())
             pass
         else:
             # skip optimizer step
@@ -213,18 +220,17 @@ class Tensorboard(Callback):
                 self.writer.add_scalar("loss/train", loss, self.train_batch_count)
             except:
                 pass
-            if not self.ddp:
-                self.writer.add_scalar(
-                    "data/seqlen/x", self.learn.xb[0][0].size(1), self.train_batch_count
-                )
-                self.writer.add_scalar(
-                    "data/seqlen/y", self.learn.xb[0][1].size(1), self.train_batch_count
-                )
-                self.writer.add_scalar(
-                    "data/batch_size",
-                    self.learn.xb[0][0].size(0),
-                    self.train_batch_count,
-                )
+            self.writer.add_scalar(
+                "data/seqlen/x", self.learn.xb[0][0].size(1), self.train_batch_count
+            )
+            self.writer.add_scalar(
+                "data/seqlen/y", self.learn.xb[0][1].size(1), self.train_batch_count
+            )
+            self.writer.add_scalar(
+                "data/batch_size",
+                self.learn.xb[0][0].size(0),
+                self.train_batch_count,
+            )
 
     def after_step(self):
         if self.steps % LOG_EVERY_N_STEPS == 0:

libreasr/lib/layers/stacked_rnn.py
@@ -16,6 +16,38 @@ RNN_TYPES = ["LSTM", "GRU", "NBRC"]
 USE_PYTORCH = True
 
 
+class ScaleNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-5):
+        super().__init__()
+        self.scale = dim ** -0.5
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(1))
+
+    def forward(self, x):
+        norm = torch.norm(x, dim = -1, keepdim = True) * self.scale
+        return x / norm.clamp(min = self.eps) * self.g
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim, eps = 1e-8):
+        super().__init__()
+        self.scale = dim ** -0.5
+        self.eps = eps
+        self.g = nn.Parameter(torch.ones(dim))
+
+    def forward(self, x):
+        norm = torch.norm(x, dim = -1, keepdim = True) * self.scale
+        return x / norm.clamp(min = self.eps) * self.g
+
+
+NORMS = {
+    "bn": nn.BatchNorm1d,
+    "ln": nn.LayerNorm,
+    "sn": ScaleNorm,
+    "rn": RMSNorm,
+}
+
+
 def get_rnn_impl(rnn_type):
     return getattr(nn, rnn_type)
 
@@ -84,10 +116,11 @@ class StackedRNN(nn.Module):
 
         # state cache (key: bs, value: state)
         self.cache = {}
+        self.reinit_cache = True
 
         # norm (BN or LN)
         self.norm = norm
-        norm_cls = nn.BatchNorm1d if norm == "bn" else nn.LayerNorm
+        norm_cls = NORMS[norm]
         self.bns = nn.ModuleList()
         for i, o in zip(self._is, self._os):
             n = norm_cls(o)
@@ -152,6 +185,9 @@ class StackedRNN(nn.Module):
             return self.rnns[i](x, s, lengths=lengths if lengths is not None else None)
 
     def forward(self, x, state=None, lengths=None):
+        if self.reinit_cache:
+            self.cache = {}
+            self.reinit_cache = False
         bs = x.size(0)
         residual = 0.0
         new_states = []

libreasr/lib/loss.py
@@ -80,7 +80,7 @@ def get_loss_func(
 
         # preprocess inp_lens
         # factor reduction in
-        inp_lens = torch.clamp(inp_lens // reduction_factor, min=1, max=inp.size(1))
+        inp_lens = torch.clamp((inp_lens // reduction_factor) - 1, min=1, max=inp.size(1))
 
         # avoid NaN
         if zero_nan:

libreasr/lib/models.py
@@ -68,25 +68,31 @@ class Preprocessor(Module):
     def __init__(self):
         from nnAudio.Spectrogram import MelSpectrogram
 
+        sr = 8000 # 16000
+        n_mels = 64 # 128
+        n_fft = 1024 # 2048
         self.spec = MelSpectrogram(
-            sr=16000,
+            sr=sr,
             trainable_mel=True,
             trainable_STFT=True,
-            n_fft=2048,
-            n_mels=128,
-            win_length=400,
-            hop_length=160,
+            n_fft=n_fft,
+            n_mels=n_mels,
+            win_length=int(0.025 * sr),
+            hop_length=int(0.01 * sr),
         )
+        self.sr = sr
+        self.downsample = 8
 
     def param_groups(self):
         return [p for p in self.parameters() if p.requires_grad]
 
     def forward(self, x, xl=None):
         x = self.spec(x[..., :, 0, 0]).permute(0, 2, 1).contiguous()
-        x = x.unfold(-2, 10, 8).contiguous()
+        x = x.unfold(-2, 10, self.downsample).contiguous()
         x = x.view(x.size(0), x.size(1), -1).contiguous()
         if xl is not None:
-            xl = torch.clamp(xl // (160 * 8), min=0, max=x.size(1))
+            fac = self.sr // 100
+            xl = torch.clamp(xl // (fac * self.downsample), min=1, max=x.size(1))
             return x, xl
         return x
 
@@ -209,7 +215,7 @@ class Joint(Module):
         self.reversible = reversible
         if joint_method == "add":
             input_sz = out_sz
-        elif joint_method == "concat":
+        elif joint_method == "concat" or self.joint_method == "comb":
             input_sz = 2 * out_sz
         else:
             raise Exception("No such joint_method")
@@ -232,6 +238,12 @@ class Joint(Module):
             )
             self.joint = inv_joint
         else:
+            # less memory usage
+            # self.joint = nn.Sequential(
+            #     nn.Tanh(),
+            #     nn.Linear(input_sz, vocab_sz),
+            # )
+            # custom LibreASR
             self.joint = nn.Sequential(
                 nn.Linear(input_sz, joint_sz),
                 nn.Tanh(),
@@ -256,6 +268,10 @@ class Joint(Module):
             h_enc.register_hook(lambda grad: grad / h_pred.size(1))
         if self.joint_method == "add":
             x = h_pred + h_enc
+        elif self.joint_method == "mul":
+            x = h_pred * h_enc
+        elif self.joint_method == "comb":
+            x = torch.cat((h_pred + h_enc, h_pred * h_enc), dim=-1)
         elif self.joint_method == "concat":
             x = torch.cat((h_pred, h_enc), dim=-1)
         else:
@@ -445,9 +461,12 @@ class Transducer(Module):
 
         # unpack
         x, y, xl, yl = tpl
+        # print("x", x.shape)
 
         # preprocess
         x, xl = self.preprocessor(x, xl)
+        if torch.isinf(x).any() or torch.isnan(x).any():
+            print("WARN: x is invalid after preprocessor...")
 
         # encoder
         x = x.reshape(x.size(0), x.size(1), -1)

libreasr/lib/transforms.py
@@ -65,8 +65,12 @@ torchaudio.set_audio_backend("soundfile")
 
 
 class OpenAudioSpan(Transform):
-    def __init__(self, tpls):
+    def __init__(self, tpls, pad_end, pad_factor=2.25, sr=16000, **kwargs):
         self.tpls = tpls
+        self.pad_end = pad_end
+        self.pad_factor = pad_factor
+        self.pad_tensor_full  = torch.zeros(1, int(pad_end*sr))
+        self.pad_tensor_empty = torch.zeros(1, 0)
 
     def encodes(self, i) -> AudioTensor:
         tpl = self.tpls[i]
@@ -84,20 +88,35 @@ class OpenAudioSpan(Transform):
             use_sr_csv = True
             sr = sr_csv
 
-        # crop
+        # crop slice with padding
         xstart = TupleGetter.xstart(tpl)
         if not math.isnan(xstart):
             xstart = int((xstart / 1000.0) * sr)
         else:
             xstart = 0
-        pad = int(0.5 * sr)
+        pad = int(self.pad_end * self.pad_factor * sr)
         xlen = TupleGetter.xlen(tpl)
         if int(xlen) == -1 or math.isnan(xlen):
             xlen = 800000
         else:
             xlen = int((xlen / 1000.0) * sr) + pad
         sig, sr_sig = torchaudio.load(fname, frame_offset=xstart, num_frames=xlen)
-        return AudioTensor(sig[:1, :], sr=sr_csv if use_sr_csv else sr_sig)
+
+        # crop to one channel
+        sig = sig[:1, :]
+
+        # determine padding
+        #  when available, we'll just use
+        #  the continuation of the sample
+        if xstart == 0:
+            pad_tensor = self.pad_tensor_full
+        else:
+            pad_tensor = self.pad_tensor_empty
+
+        # concat
+        sig = torch.cat([sig, pad_tensor], dim=-1)
+
+        return AudioTensor(sig, sr=sr_csv if use_sr_csv else sr_sig)
 
 
 class MyOpenAudio(Transform):
@@ -105,7 +124,7 @@ class MyOpenAudio(Transform):
 
     def __init__(self, tpls, **kwargs):
         self.tpls = tpls
-        self.oa = OpenAudioSpan(tpls)
+        self.oa = OpenAudioSpan(tpls, **kwargs)
 
     def encodes(self, i) -> AudioTensor:
         try:
@@ -134,8 +153,8 @@ class ChannelCut(Transform):
 class Resample(Transform):
     order = 2
 
-    def __init__(self, target_sr, **kwargs):
-        self.sr = target_sr
+    def __init__(self, resample_sr, **kwargs):
+        self.sr = resample_sr
 
     def encodes(self, i: AudioTensor) -> AudioTensor:
         debug(self)